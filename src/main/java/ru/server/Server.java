87a826295fa0cd58574b8fedd9d6e7aad2e7dbf4
IgorStovba <pro78pro36@gmail.com>
2024-02-03 19:19:25 +0300

    Init

---
+++ src/main/java/ru/server/Server.java
@@ -0,0 +1,10 @@
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!!!!!!!!!!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/ru/server/ServerTest.java
@@ -0,0 +1,19 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+
+public class ServerTest {
+    
+    @Test void testAdd() {
+        //Server obj = new Server();
+        int t = 87;
+        assert t == 87;
+    }
+
+    @Test void testAdd2() {
+        int g = 5;
+        assertEquals(5, g);
+    }
+}
