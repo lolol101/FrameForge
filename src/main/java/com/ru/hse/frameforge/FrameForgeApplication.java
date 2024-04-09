59ec482cb6fd8d0fcc676881d8e25f4239f4359b
MaksNazarov <maksim.nazaroff.main@gmail.com>
2024-04-09 11:30:22 +0300

    finally commit changes to git: mainPage additions & changes, listener communication system outlines, misc tweaks

--- src/main/java/com/ru/hse/frameforge/FrameForgeApplication.java
+++ src/main/java/com/ru/hse/frameforge/FrameForgeApplication.java
@@ -3,6 +3,7 @@ package com.ru.hse.frameforge;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +13,11 @@ public class FrameForgeApplication extends Application {
     public void start(Stage stage) throws IOException {
         stage.setTitle("FrameForge");
 
-        FXMLLoader fxmlLoaderRegistration = new FXMLLoader(getClass().getResource("view/RegistrationView.fxml"));
-        // TODO: listeners go here?
-        Scene sceneRegistration = new Scene(fxmlLoaderRegistration.load(), 640, 480);
+//        FXMLLoader fxmlLoaderRegistration = new FXMLLoader(getClass().getResource("view/RegistrationView.fxml"));
+//        Scene sceneRegistration = new Scene(fxmlLoaderRegistration.load(), 640, 480);
+
+        FXMLLoader fxmlLoaderMain = new FXMLLoader(getClass().getResource("view/MainPageView.fxml"));
+        Scene sceneRegistration = new Scene(fxmlLoaderMain.load(), 640, 480);
 
         stage.setScene(sceneRegistration);
         stage.show();
