b8142bfbb6f3f25a97eb208e8afc09981c53f67b
MaksNazarov <maksim.nazaroff.main@gmail.com>
2024-06-25 16:15:13 +0300

    Clean up main page TODOs, fix async image load

--- src/main/java/frameforge/view/MainPageController.java
+++ src/main/java/frameforge/view/MainPageController.java
@@ -2,7 +2,9 @@ package frameforge.view;
 
 import frameforge.model.MainPageModel;
 import frameforge.viewmodel.MainPageViewModel;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
@@ -30,6 +32,8 @@ import static java.lang.Thread.sleep;
 public class MainPageController extends Controller<MainPageModel, MainPageViewModel> {
     private static final double FULLSIZE_IMG_FACTOR = 0.9;
     private static final double SCROLLBAR_MAX_VALUE_BEFORE_UPDATE = 0.95;
+    private static final double imageInFeedWidth = 600;
+    private static final double imageInFeedHeight = 400;
     @FXML
     private HBox fullSizePane;
     @FXML
@@ -49,6 +53,7 @@ public class MainPageController extends Controller<MainPageModel, MainPageViewMo
     @FXML
     private ScrollPane scrollPane;
 
+    private Task<Boolean> getNewPostTask;
     private final ChangeListener<ClientCommands> clientCommandReceiver = (obs, oldCommand, newCommand) -> {
         System.out.println("mainPageView: changeListener fired on client command reception: oldCommand=" + oldCommand + ", newCommand=" + newCommand);
         switch (newCommand) {
@@ -128,6 +133,11 @@ public class MainPageController extends Controller<MainPageModel, MainPageViewMo
             }
             // TODO: remove and bind to a StackPane? HBox? containing ScrollPane
         });
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ESCAPE) {
+                fullSizePane.setVisible(false);
+            }
+        });
     }
 
     private void sendRequestGetNextImage() { // TODO: check if it works at all
@@ -147,141 +157,137 @@ public class MainPageController extends Controller<MainPageModel, MainPageViewMo
     }
 
     private void loadNextImageBatchFromModel() {
-        // TODO: rewrite to use CompletableFuture!
-        try {
-            Pair<String, List<Image>> nextIdAndImage = getNextPost();
-            List<Image> images = nextIdAndImage.getValue();
-            ImageView imageView = new ImageView(images.getFirst());
-            imageView.setPreserveRatio(true);
-
-            double imageViewWidth = 600; // TODO: to static const?
-            double imageViewHeight = 400;
-
-            setFitCustom(imageView, imageViewWidth, imageViewHeight);
-
-            StackPane imagePane = new StackPane(imageView);
-            imagePane.setPrefSize(imageViewWidth, imageViewHeight);
-            imagePane.setStyle("-fx-background-color: #d1e1d4;");
-
-            Button nextImageButton = new Button("->"); // TODO: switch to free-use icons
-            nextImageButton.getStyleClass().add("post-button");
-            Button previousImageButton = new Button("<-"); // TODO: switch to free-use icons
-            previousImageButton.getStyleClass().add("post-button");
-
-            // TODO: position image switch buttons on mid-height
-            // TODO: set them visible if courser is on the sides of image
-
-            nextImageButton.setOnAction(event -> {
-                int currentImageIndex = images.indexOf(imageView.getImage());
-                int nextImageIndex = (currentImageIndex + 1) % images.size();
-                imageView.setImage(images.get(nextImageIndex));
-                setFitCustom(imageView, imageViewWidth, imageViewHeight);
-            });
-            previousImageButton.setOnAction(event -> {
-                int currentImageIndex = images.indexOf(imageView.getImage());
-                int nextImageIndex = (currentImageIndex - 1) % images.size();
-                if (nextImageIndex < 0) nextImageIndex += images.size();
-                imageView.setImage(images.get(nextImageIndex));
-                setFitCustom(imageView, imageViewWidth, imageViewHeight);
-            });
-
-            ToggleButton likeButton = new ToggleButton();
-            Button commentButton = new Button("Comment");
-            Button saveButton = new Button("Save");
-
-            likeButton.getStyleClass().add("like-button");
-            Image imgIsNotLiked = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon-like.png")));
-            ImageView imgViewNotLiked = new ImageView(imgIsNotLiked);
-            imgViewNotLiked.setPreserveRatio(true);
-            imgViewNotLiked.setFitHeight(10);
-            likeButton.setGraphic(imgViewNotLiked);
-
-            Image imgIsLiked = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon-unlike.png")));
-            ImageView imgViewLiked = new ImageView(imgIsLiked);
-            imgViewLiked.setPreserveRatio(true);
-            imgViewLiked.setFitHeight(10);
-            likeButton.selectedProperty().addListener((observable, oldValue, newValue) -> {
-                if (newValue) {
-                    likeButton.setGraphic(imgViewLiked);
-                } else {
-                    likeButton.setGraphic(imgViewNotLiked);
-                }
-            });
-
-
-            commentButton.getStyleClass().add("post-button");
-            saveButton.getStyleClass().add("post-button");
-
-            HBox contextButtonContainer = new HBox(20, previousImageButton, likeButton, commentButton, saveButton, nextImageButton);
-            contextButtonContainer.setAlignment(Pos.BOTTOM_CENTER);
-            contextButtonContainer.setTranslateY(-10); // a tad higher than image border
-
-            imagePane.setOnMouseEntered(event -> {
-                contextButtonContainer.setVisible(true);
-            });
-            imagePane.setOnMouseExited(event -> {
-                contextButtonContainer.setVisible(false);
-            });
-
-            likeButton.setOnAction(event -> sendRequestLikeOrUnlikePost(nextIdAndImage.getKey()));
-            saveButton.setOnAction(event -> sendRequestSavePic(nextIdAndImage.getKey(), images.indexOf(imageView.getImage())));
-
-            imagePane.getChildren().add(contextButtonContainer);
-            contextButtonContainer.setVisible(false);
-
-            imagePane.setOnMouseClicked(event -> {
-                System.out.println("Clicked on an image!");
-                Image imageToLoad = imageView.getImage();
-                fullSizeImageView.setImage(imageToLoad);
-
-                Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-                double screenWidth = screenBounds.getWidth();
-                double screenHeight = screenBounds.getHeight();
-
-                double imageWidth = imageToLoad.getWidth();
-                double imageHeight = imageToLoad.getHeight();
-
-                double aspectRatio = imageWidth / imageHeight;
-                double optimalWidth = screenWidth * FULLSIZE_IMG_FACTOR;
-                double optimalHeight = optimalWidth / aspectRatio;
-
-                if (optimalHeight > screenHeight * FULLSIZE_IMG_FACTOR) {
-                    optimalHeight = screenHeight * FULLSIZE_IMG_FACTOR;
-                    optimalWidth = optimalHeight * aspectRatio;
-                }
-
-                fullSizeImageView.setFitWidth(optimalWidth);
-                fullSizeImageView.setFitHeight(optimalHeight);
-
-                fullSizePane.setPrefSize(optimalWidth, optimalHeight);
-                fullSizePane.getChildren().clear();
-                fullSizePane.getChildren().add(fullSizeImageView);
-                fullSizePane.setVisible(true);
-            });
-
-            fullSizePane.setOnMouseClicked(event -> {
-                if (!event.getTarget().equals(imageView)) {
-                    fullSizePane.setVisible(false);
-                }
-            });
-
-            scene.setOnKeyPressed(event -> {
-                if (event.getCode() == KeyCode.ESCAPE) {
-                    fullSizePane.setVisible(false);
-                }
-            });
-
-            postsTilePane.getChildren().add(imagePane);
-
-            System.err.println("Next post loaded");
-        } catch (NullPointerException e) {
-            System.err.println("error when trying to load an image: please check path settings and model methods' errors");
+        if (getNewPostTask != null && getNewPostTask.isRunning()) {
+            System.err.println("Next post is already being loaded");
+            return;
         }
+        getNewPostTask = new Task<>() {
+            @Override
+            protected Boolean call() {
+                try {
+                    Pair<String, List<Image>> nextIdAndImage = getNextPost();
+                    List<Image> images = nextIdAndImage.getValue();
+                    ImageView imageView = new ImageView(images.getFirst());
+                    imageView.setPreserveRatio(true);
+
+                    setFitCustom(imageView, imageInFeedWidth, imageInFeedHeight);
+
+                    StackPane imagePane = new StackPane(imageView);
+                    imagePane.setPrefSize(imageInFeedWidth, imageInFeedHeight);
+                    imagePane.setStyle("-fx-background-color: #d1e1d4;");
+
+                    Button nextImageButton = new Button("->"); // TODO: switch to free-use icons
+                    nextImageButton.getStyleClass().add("post-button");
+                    Button previousImageButton = new Button("<-"); // TODO: switch to free-use icons
+                    previousImageButton.getStyleClass().add("post-button");
+
+                    // TODO: position image switch buttons on mid-height
+                    // TODO: set them visible if courser is on the sides of image
+
+                    nextImageButton.setOnAction(event -> {
+                        int currentImageIndex = images.indexOf(imageView.getImage());
+                        int nextImageIndex = (currentImageIndex + 1) % images.size();
+                        imageView.setImage(images.get(nextImageIndex));
+                        setFitCustom(imageView, imageInFeedWidth, imageInFeedHeight);
+                    });
+                    previousImageButton.setOnAction(event -> {
+                        int currentImageIndex = images.indexOf(imageView.getImage());
+                        int nextImageIndex = (currentImageIndex - 1) % images.size();
+                        if (nextImageIndex < 0) nextImageIndex += images.size();
+                        imageView.setImage(images.get(nextImageIndex));
+                        setFitCustom(imageView, imageInFeedWidth, imageInFeedHeight);
+                    });
+
+                    ToggleButton likeButton = new ToggleButton();
+                    Button saveButton = new Button("Save");
+
+                    likeButton.getStyleClass().add("like-button");
+                    Image imgIsNotLiked = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon-like.png")));
+                    ImageView imgViewNotLiked = new ImageView(imgIsNotLiked);
+                    imgViewNotLiked.setPreserveRatio(true);
+                    imgViewNotLiked.setFitHeight(10);
+                    likeButton.setGraphic(imgViewNotLiked);
+
+                    Image imgIsLiked = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon-unlike.png")));
+                    ImageView imgViewLiked = new ImageView(imgIsLiked);
+                    imgViewLiked.setPreserveRatio(true);
+                    imgViewLiked.setFitHeight(10);
+                    likeButton.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                        if (newValue) {
+                            likeButton.setGraphic(imgViewLiked);
+                        } else {
+                            likeButton.setGraphic(imgViewNotLiked);
+                        }
+                    });
+
+                    saveButton.getStyleClass().add("post-button");
+
+                    HBox contextButtonContainer = new HBox(20, previousImageButton, likeButton, saveButton, nextImageButton);
+                    contextButtonContainer.setAlignment(Pos.BOTTOM_CENTER);
+                    contextButtonContainer.setTranslateY(-10); // a tad higher than image border
+
+                    imagePane.setOnMouseEntered(event -> contextButtonContainer.setVisible(true));
+                    imagePane.setOnMouseExited(event -> contextButtonContainer.setVisible(false));
+
+                    likeButton.setOnAction(event -> sendRequestLikeOrUnlikePost(nextIdAndImage.getKey()));
+                    saveButton.setOnAction(event -> sendRequestSavePic(nextIdAndImage.getKey(), images.indexOf(imageView.getImage())));
+
+                    imagePane.getChildren().add(contextButtonContainer);
+                    contextButtonContainer.setVisible(false);
+
+                    imagePane.setOnMouseClicked(event -> {
+                        System.out.println("Clicked on an image!");
+                        Image imageToLoad = imageView.getImage();
+                        fullSizeImageView.setImage(imageToLoad);
+
+                        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+                        double screenWidth = screenBounds.getWidth();
+                        double screenHeight = screenBounds.getHeight();
+
+                        double imageWidth = imageToLoad.getWidth();
+                        double imageHeight = imageToLoad.getHeight();
+
+                        double aspectRatio = imageWidth / imageHeight;
+                        double optimalWidth = screenWidth * FULLSIZE_IMG_FACTOR;
+                        double optimalHeight = optimalWidth / aspectRatio;
+
+                        if (optimalHeight > screenHeight * FULLSIZE_IMG_FACTOR) {
+                            optimalHeight = screenHeight * FULLSIZE_IMG_FACTOR;
+                            optimalWidth = optimalHeight * aspectRatio;
+                        }
+
+                        fullSizeImageView.setFitWidth(optimalWidth);
+                        fullSizeImageView.setFitHeight(optimalHeight);
+
+                        fullSizePane.setPrefSize(optimalWidth, optimalHeight);
+                        fullSizePane.getChildren().clear();
+                        fullSizePane.getChildren().add(fullSizeImageView);
+                        fullSizePane.setVisible(true);
+                    });
+
+                    fullSizePane.setOnMouseClicked(event -> {
+                        if (!event.getTarget().equals(imageView)) {
+                            fullSizePane.setVisible(false);
+                        }
+                    });
+
+                    Platform.runLater(() -> postsTilePane.getChildren().add(imagePane));
+                    System.err.println("Next post loaded");
+                    return true;
+                } catch (NullPointerException e) {
+                    System.err.println("error when trying to load an image: please check path settings and model methods' errors");
+                    return false;
+                }
+            }
+        };
+
+        Thread thread = new Thread(getNewPostTask);
+        thread.setDaemon(true);
+        thread.start();
     }
 
     private Pair<String, List<Image>> getNextPost() {
         return viewModel.getNextPost();
-        // TODO: work with image getters, names & anything else goes here
     }
 
     @FXML
@@ -296,10 +302,6 @@ public class MainPageController extends Controller<MainPageModel, MainPageViewMo
 
     private void sendRequestLikeOrUnlikePost(String postID) {
         viewModel.like(postID);
-    }
-
-    private void sendRequestCommentOnPost(String postID) {
-
     }
 
     private void sendRequestSavePic(String postID, Integer picNum) {
